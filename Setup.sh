e60e5ff264a9205277230c97e91c3d067966cf07
Ashraful Islam <ashraful1185058@gmail.com>
2024-11-25 18:47:27 +0600

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -344,10 +344,48 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
+echo "--------------------------------------------------"
+echo "----------------------Finished Installing C++----------------------------"
+fi
+
+echo "Do you want to Install ||---> Postgresql  <---||,If want press ||---> y <---|| or press ||---> n <---||to continue"
+read press
+ if [ $press == "y" ];then
+echo "Yeah !! Started to install Postgres"
+echo "--------------------------------------------------"
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql.service
+
+echo "--------------------------------------------------"
+echo "----------------------Finished Installing Postgres----------------------------"
+fi
+
+echo "Do you want to Install ||---> pgadmin  <---||,If want press ||---> y <---|| or press ||---> n <---||to continue"
+read press
+ if [ $press == "y" ];then
+echo "Yeah !! Started to install pgadmin"
+echo "--------------------------------------------------"
+sudo apt update
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
+echo "--------------------------------------------------"
+echo "----------------------Finished Installing Postgres----------------------------"
+fi
+
+echo "Do you want to Install ||---> postman  <---||,If want press ||---> y <---|| or press ||---> n <---||to continue"
+read press
+ if [ $press == "y" ];then
+echo "Yeah !! Started to install Postman"
+echo "--------------------------------------------------"
+sudo apt update
+curl -O https://dl.pstmn.io/download/latest/linux_64
+cd Downloads
+	sudo rm -rf /opt/Postman/
 
 
 echo "--------------------------------------------------"
-latex --version
-echo "----------------------Finished Installing C++----------------------------"
+echo "----------------------Finished Installing Postgres----------------------------"
 fi
 
